227689f140c4085f50bed770b34ab83430260120
Jasper <19375788+CactusJasper@users.noreply.github.com>
2021-03-13 09:13:37 +0000

    added config.copy.js and database.copy.js files

---
+++ webServer/config/config.copy.js
@@ -0,0 +1,11 @@
+module.exports = {
+    secret: '',
+    jwt_secret: '',
+    secret_store: '',
+    discord: {
+        clientID: '',
+        clientSecret: '',
+        callbackURI: 'http://localhost:8080/auth/discord/callback'
+    },
+    server_id: '803358616470945881'
+};
