512a2c9b121a432ce962d7a90ac32b8349917520
Jasper <19375788+CactusJasper@users.noreply.github.com>
2021-04-01 13:02:13 +0100

    general cleanup

--- bot/modules/moderation.js
+++ bot/modules/moderation.js
@@ -1,28 +1,12 @@
-let Sentiment = require('sentiment');
-let sentiment = new Sentiment();
-let fs = require('fs');
 let tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 let toxicity = require('@tensorflow-models/toxicity');
-const cld = require('cld');
 const aposToLexForm = require('apos-to-lex-form');
 const natural = require('natural');
 const SW = require('stopword');
 const SpellCorrector = require('spelling-corrector');
 let spellCorrector = new SpellCorrector();
 spellCorrector.loadDictionary();
-
-let options = {
-    extras: {
-        'wtf': 0,
-        'fuck': -1,
-        'bitch': -1,
-        'cock': -1,
-        'faggot': -2,
-        'kill': -4,
-        'yourself': -3
-    }
-}
 
 const threshold = 0.9;
 let model;
@@ -82,52 +66,4 @@ exports.isSafeMessage = (message) => {
     {
         this.loadModel();
     }
-
-    //let result = sentiment.analyze(message.content, options);
-    //if(result.comparative < -3 && result.score < -3)
-    //{
-        /*toxicity.load(threshold).then(model => {
-            model.classify(message.content).then(predictions => {
-                let toxic = false;
-
-                for(let i = 0; i < predictions.length; i++)
-                {
-                    if(predictions[i].label == 'toxicity' || predictions[i].label == 'severe_toxicity' || predictions[i].label == 'threat')
-                    {
-                        if(predictions[i].results[0].match == true)
-                        {
-                            toxic = true;
-                        }
-                    }
-                }
-
-                if(toxic)
-                {
-                    let logs = JSON.parse(fs.readFileSync('./log.json'));
-        
-                    logs.push({
-                        //result: result,
-                        predictions: predictions
-                    });
-                    fs.writeFileSync('./log.json', JSON.stringify(logs, null, 4));
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }).catch(err => {
-                console.error(err);
-                return true;
-            });
-        }).catch(err => {
-            console.error(err);
-            return true;
-        });*/
-        
-    //}
-    //else
-    //{
-    //    return true;
-    //}
 }
