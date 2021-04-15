cb3fa6283006d515bce84e7f19ef5edf7f5b7bf4
Jasper <19375788+CactusJasper@users.noreply.github.com>
2021-04-15 01:38:43 +0100

    finished rpg_item model

--- bot/models/rpg_item.js
+++ bot/models/rpg_item.js
@@ -8,5 +8,23 @@ let rpgItemSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    stats: [{
+        stat_id: {
+            type: Number,
+            required: true
+        },
+        stat_name: {
+            type: String,
+            required: true
+        },
+        effect_on_stat: {
+            type: Number,
+            default: 1
+        },
+        debuf: {
+            type: Boolean,
+            default: false
+        }
+    }]
 });
