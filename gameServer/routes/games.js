646a5722dd1461bae9dd2f8383af54a6f91158b9
Jasper <19375788+CactusJasper@users.noreply.github.com>
2021-07-08 22:44:20 +0100

    Removed Game Server Fully
    Did a full removal at request to downscale and reduce hardware requirements

--- gameServer/routes/games.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let middleware = require('../utils/middleware');
-
-router.get('/2048', middleware.ensureAuthenticated, (req, res) => {
-    res.render('games/2048');
-});
-
-module.exports = router;
