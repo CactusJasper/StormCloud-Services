6b8e0351eb01015bb98ecc72e7cc58433c5be51a
Jasper <19375788+CactusJasper@users.noreply.github.com>
2021-07-08 23:57:26 +0100

    added super user default cause derp

--- webServer/config/config.copy.js
+++ webServer/config/config.copy.js
@@ -7,5 +7,6 @@ module.exports = {
         clientSecret: '',
         callbackURI: 'http://localhost:8080/auth/discord/callback'
     },
-    server_id: '803358616470945881'
+    server_id: '803358616470945881',
+    default_super_users: ['217387293571284992', '228618507955208192']
 };
