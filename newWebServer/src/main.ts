9d30661758936d53fe3453b80eaa93e659a1ccba
Jasper <daniel.j.cowley@hotmail.co.uk>
2021-10-08 10:40:38 +0100

    basic scss setup

--- newWebServer/src/main.ts
+++ newWebServer/src/main.ts
@@ -4,14 +4,14 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
+    const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-  );
+    );
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('hbs');
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
